5449e80c3042dc0fdfb20a49c54e34e9f69faa71
waspryo <wasp2123321@gmail.com>
2019-01-21 18:48:06 +0900

    deplpy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-     @post = Post.find(params[:id])
+     @posts = Post.find(params[:id])
   end
 
   # GET /posts/new
